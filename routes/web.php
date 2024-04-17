18bc83b094cf93a37baf600009231581af6151e8
Douglas White <douglas.rw@outlook.com>
2024-04-17 15:14:41 -0600

    Created Post controller, table and model. Created Route for creating, storing and showing a post.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
+// user routes
 Route::get('/', [UserController::class, "showCorrectHomepage"]);
-
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout']);
+
+// blog post routes
+Route::get('/create-post', [PostController::class, 'showCreateForm']);
+Route::post('/create-post', [PostController::class, 'storeNewPost']);

c974679bd56d118748d9896f38fd7e532104f22f
Douglas White <douglas.rw@outlook.com>
2024-04-18 17:39:14 -0600

    Change delete policy and edit over to middleware approach. Created routes for show and edit.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
 Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('auth');
 Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('auth');
 Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
-Route::delete('/post/{post}', [PostController::class, 'delete']);
+Route::delete('/post/{post}', [PostController::class, 'delete'])->middleware('can:delete,post');
+Route::get('/post/{post}/edit', [PostController::class, 'showEditForm'])->middleware('can:update,post');
+Route::put('/post/{post}', [PostController::class, 'actuallyUpdate'])->middleware('can:update,post');
+
 
 // profile routes
 Route::get('/profile/{user:username}', [UserController::class, 'profile']);

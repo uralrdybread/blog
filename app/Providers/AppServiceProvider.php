6ed9da73563c87df63c597cb9cd48416ffba5825
Douglas White <douglas.rw@outlook.com>
2024-04-22 20:55:53 -0600

    Created pagination for posts by followed users. enabled bootstrap by default in appservices

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Paginator::useBootstrapFive();
+
         Gate::define('visitAdminPages', function($user) {
             return $user->isAdmin === 1;
         });
